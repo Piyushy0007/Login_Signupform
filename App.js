307c74aef743c06aa85b3130cda0f27a26573c83
Piyushy0007 <142148156+Piyushy0007@users.noreply.github.com>
2024-01-21 00:08:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
+
+import './App.css'
+
+function App() {
+    
+    return (
+        <>
+        
+        <Router>
+            <div>
+                <Routes>
+                
+                    <Route exact path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={ <RegisterPage/> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+               
+            </div>
+        </Router>
+        </>
+    )
+}
+
+
+
+
+export default App;
